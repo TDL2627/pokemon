52a688e47d40d3c880e3ca8d48e2814fb543ecf1
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-03 09:51:26 +0200

    created an object of arrays

--- coffee.js
+++ coffee.js
@@ -1,4 +1,4 @@
-let pokemon = [
+let pokemons = [
     {
         Ppic:"https://picsum.photos/300?random=1",
         name:"Charizard",
@@ -6,6 +6,20 @@ let pokemon = [
         details:"The final form of charmander looks like a dragon but isnt."
     },
     {
-
+        Ppic:"https://picsum.photos/300?random=2",
+        name:"Lucario",
+        type:"Dark",
+        details:"The final for of Riolu .Can fight like a ryu from street fighter"
     }
-]
+];
+function createPokeball(pokeball) {
+    let createdPokeball = `<div class="pokemon">
+        <img src="${pokeball.Ppic}">
+        <h3>${pokeball.name}</h3>
+        <h5>${card.type}</h5>
+        <p>${card.details}</p>
+
+      </div>
+    `;
+    return createdPokeball;
+  }
