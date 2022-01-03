65ab954c770207bfb8137aa1c85990220a16fa90
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-03 09:26:58 +0200

    first upload

---
+++ coffee.js
@@ -0,0 +1,11 @@
+let pokemon = [
+    {
+        Ppic:"https://picsum.photos/300?random=1",
+        name:"Charizard",
+        type:"fire",
+        details:"The final form of charmander looks like a dragon but isnt."
+    },
+    {
+
+    }
+]
