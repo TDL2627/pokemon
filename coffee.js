4496869c5489da0857e1f1258f082fc26456ff83
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-03 11:21:36 +0200

    final styling for now

--- coffee.js
+++ coffee.js
@@ -1,23 +1,43 @@
 let pokemon = [
     {
-        Ppic:"https://picsum.photos/300?random=1",
+        Ppic:"/images/charizard.png",
         name:"Charizard",
-        type:"fire",
-        details:"The final form of charmander looks like a dragon but isnt."
+        type:"Fire",
+        details:"The final form of Charmander looks like a dragon but isnt."
     },
     {
-        Ppic:"https://picsum.photos/300?random=2",
+        Ppic:"/images/greninja.png",
+        name:"Greninja",
+        type:"Water",
+        details:"The final form of Froakie.A ninja frog."
+    },
+    {
+        Ppic:"/images/lucario.png",
         name:"Lucario",
         type:"Dark",
-        details:"The final for of Riolu .Can fight like a ryu from street fighter"
+        details:"The final form of Riolu .Can fight like a ryu from street fighter."
+    },
+    {
+        Ppic:"/images/zekrom.png",
+        name:"Zekrom",
+        type:"Lightning",
+        details:"A legendary pokemon .A electric dragon."
+    },
+    {
+        Ppic:"/images/arceus.png",
+        name:"Arceus",
+        type:"god",
+        details:"The god of pokemon.All types can beat anything."
     }
 ];
 function createPokeball(pokeball) {
-    let createdPokeball = `<div class="pokeball">
-        <img src="${pokeball.Ppic}">
+    let createdPokeball = `
+    <div class="pokeball">
+        <img class="pic" src="${pokeball.Ppic}">
         <h3>${pokeball.name}</h3>
         <h5>${pokeball.type}</h5>
         <p>${pokeball.details}</p>
+      </div>
       </div>
     `;
     return createdPokeball;
